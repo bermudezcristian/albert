335ea3b1dc406cb09f6930500db1b3d20f35e4f1
Cristian Bermudez <cristiangastonbermudez@gmail.com>
2021-04-03 22:28:16 -0300

    Adding powerlevel10k

---
+++ run_personalized.sh
@@ -0,0 +1,8 @@
+export GIT_USER_NAME="Cristian Bermudez"
+export GIT_USER_EMAIL="cristiangastonbermudez@gmail.com"
+export GIT_COMMIT_USE_GPG="true"
+export GIT_USER_GPG_KEY="616552BD9A1E986C"
+export MACOS_NAME="albert"
+export HOME_DIRECTORY="/Users/cristianbermudez"
+export FIREFOX_PERSONALIZATION="true"
+/bin/bash run.sh
